15446f6863b44932734282393fc37c8347dde416
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 14:55:51 -0600

    creates a Views folder for Mainpage.js

---
+++ src/components/Views/Mainpage.js
@@ -0,0 +1,15 @@
+import HeaderGeneral from "../HeaderGeneral";
+import NavBar from "../NavBar";
+import MainGrid from "../MainGrid";
+
+const Mainpage = () => {
+    return(
+        <>
+            <HeaderGeneral/>
+            <NavBar/>
+            <MainGrid/>
+       </>
+    )
+}
+
+export default Mainpage;
