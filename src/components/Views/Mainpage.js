47d2fcad53c027ed5675aef93e93702c6791f88e
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 16:54:39 -0600

    adds the readPosts function using axiosGet from api.js and pass it as a prop to MainGrid.js

--- src/components/Views/Mainpage.js
+++ src/components/Views/Mainpage.js
@@ -2,13 +2,29 @@ import '../styles/Mainpage.css'
 import HeaderGeneral from "../HeaderGeneral";
 import NavBar from "../NavBar";
 import MainGrid from "../MainGrid";
+import { useEffect, useState } from "react";
+import { axiosGet } from "../../services/api";
+import axios from 'axios';
 
 const Mainpage = () => {
+    const [dataPosts, setDataPosts] = useState([]);
+
+    const URL_POST = '/posts';
+
+    const readPosts = () => {
+        axiosGet(URL_POST)
+        .then((result) => setDataPosts(result.data))
+    }
+
+    useEffect(()=>{
+        readPosts();
+    }, [])
+
     return(
-        <div class="grid-general">
+        <div className="grid-general">
             <HeaderGeneral/>
             <NavBar/>
-            <MainGrid/>
+            <MainGrid posts={dataPosts}/>
         </div>
     )
 }
