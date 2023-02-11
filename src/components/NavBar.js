e071b9cc1ae836ff79623c2f8f343b5cab92396f
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 14:57:32 -0600

    creates the nav bar for the page

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () =>{
+    return (
+        <nav class="menu">
+            <ul>
+                <li><a href="#">All</a></li>
+                <li><a href="#">Travel</a></li>
+                <li><a href="#">Lifestyle</a></li>
+                <li><a href="#">Bussiness</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Work</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
