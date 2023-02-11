50f0917a41450ae1056e470133afd8cea8ceb26d
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 15:28:34 -0600

    adds brackets to p with class subtitle

--- src/components/HeaderGeneral.js
+++ src/components/HeaderGeneral.js
@@ -3,7 +3,7 @@ import './styles/HeaderGeneral.css';
 const HeaderGeneral = () => {
     return(
         <header class="header-general">
-            <p class="subtitle"> Make your life easier </p>
+            <p class="subtitle"> [ Make your life easier ] </p>
             <h1 class="main-title"> Discovering the World </h1>
         </header>
     )
