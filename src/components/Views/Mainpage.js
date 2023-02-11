192924521f69d612da8c99a80dddde0fb5773704
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 15:16:55 -0600

    imports the Mainpage.css into Mainpage.js

--- src/components/Views/Mainpage.js
+++ src/components/Views/Mainpage.js
@@ -1,14 +1,15 @@
+import '../styles/Mainpage.css'
 import HeaderGeneral from "../HeaderGeneral";
 import NavBar from "../NavBar";
 import MainGrid from "../MainGrid";
 
 const Mainpage = () => {
     return(
-        <>
+        <div class="grid-general">
             <HeaderGeneral/>
             <NavBar/>
             <MainGrid/>
-       </>
+        </div>
     )
 }
 
