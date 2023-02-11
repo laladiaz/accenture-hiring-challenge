bb00219c8b630536df5a345b2fd3540d353c707e
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 14:56:40 -0600

    creates the header for the page

---
+++ src/components/HeaderGeneral.js
@@ -0,0 +1,12 @@
+
+const HeaderGeneral = () => {
+
+    return(
+        <header>
+            <p> Make your life easier </p>
+            <h1> Discovering the World </h1>
+        </header>
+    )
+}
+
+export default HeaderGeneral;
