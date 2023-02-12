8307459e663957b9d80295d6e7d44099744933f9
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 19:32:54 -0600

    writes the axiosPost function in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,12 @@ import axios from "axios";
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export const axiosGet = (url) => axios.get(`${BASE_URL}${url}`);
+
+export const axiosPost = (url, body) => {
+    return axios.post(`${BASE_URL}${url}`, body, {
+        headers:{
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+
+    });
+}
