9174f5865aaf3ab40b893357bb1e63518792fd73
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 18:44:15 -0600

    creates the PostBody.js component

---
+++ src/components/PostBody.js
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+const PostBody = () => {
+
+
+    return(
+        <>
+            <NavLink to='/'>Back</NavLink>
+            <div>hola</div>
+        </>
+        
+    )
+}
+
+export default PostBody;
