63245618ee71707660c1541b0d5a7bcb622d2776
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 18:46:17 -0600

    adds the pencil for the CRUD in HeaderGeneral.js

--- src/components/HeaderGeneral.js
+++ src/components/HeaderGeneral.js
@@ -5,6 +5,7 @@ const HeaderGeneral = () => {
         <header className="header-general">
             <p className="subtitle"> [ Make your life easier ] </p>
             <h1 className="main-title"> Discovering the World </h1>
+            <button className="pencil-button">pencil</button>
         </header>
     )
 }
