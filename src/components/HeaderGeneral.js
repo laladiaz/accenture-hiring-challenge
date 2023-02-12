c89bfb28014923e6e21bc3ff501955f1086ef77c
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-11 19:35:27 -0600

    adds the state to manage the show or not show from the modal and adds the Modal component to HeaderGeneral.js

--- src/components/HeaderGeneral.js
+++ src/components/HeaderGeneral.js
@@ -1,11 +1,16 @@
 import './styles/HeaderGeneral.css';
+import { useState } from 'react';
+import Modal from './Modal';
 
 const HeaderGeneral = () => {
+    const [show, setShow] = useState(false);
+
     return(
         <header className="header-general">
             <p className="subtitle"> [ Make your life easier ] </p>
             <h1 className="main-title"> Discovering the World </h1>
-            <button className="pencil-button">pencil</button>
+            <button className="pencil-button" onClick={()=>setShow(true)}>pencil</button>
+            <Modal onClose={()=>setShow(false)} show={show}/>
         </header>
     )
 }
